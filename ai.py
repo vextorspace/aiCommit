8032e00dd4122fc589beb02236e55f79ff495e2a
Douglas Ronne <vextorspace@gmail.com>
2024-11-07 14:47:41 -0800

    now we have a basic prompt scaffold

---
+++ ai.py
@@ -0,0 +1,6 @@
+class Ai:
+    def __init__(self):
+        pass
+
+    def get_commit_message(self, prompt):
+        pass
--- commit_prompt.py
+++ commit_prompt.py
@@ -1,6 +1,28 @@
-class CommitPrompt:
-    def __init__(self):
-        pass
+import unittest
+from unittest.mock import MagicMock
+from ai import Ai
 
-    def get_commit_message(self):
-        pass
+class CommitPrompt:
+    PROMPT = "::PROMPT:: with: "
+
+    def __init__(self, ai):
+        self.ai = ai
+
+    def get_commit_message(self, diff):
+        return self.ai.get_commit_message(f"{self.prompt(diff)}")
+
+    def prompt(self, diff):
+        return f"{self.PROMPT}{diff}"
+
+
+class CommitPromptTest(unittest.TestCase):
+    def test_diff_packaged_with_prompt_to_ai(self):
+        mockAi = MagicMock(spec=Ai)
+        mockAi.get_commit_message.return_value = "::FAKE COMMIT MESSAGE::"
+        commit_prompt = CommitPrompt(mockAi)
+        assert(commit_prompt.get_commit_message("::DIFF::") == "::FAKE COMMIT MESSAGE::")
+        expectedPrompt = f"{CommitPrompt.PROMPT}::DIFF::"
+        mockAi.get_commit_message.assert_called_once_with(expectedPrompt)
+
+if __name__ == '__main__':
+    unittest.main()
